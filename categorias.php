0bd42a4754e3c062abced1b4e32ea2e422a8b079
fabricabj <colavella22@gmail.com>
2022-09-10 04:21:12 -0300

    titulo cat

--- categorias.php
+++ categorias.php
@@ -15,12 +15,14 @@
          <?php 
               $select=mysqli_query($conexion,"SELECT * FROM categoria");
               while($r=mysqli_fetch_array($select)){
-         ?>
-                <div class="col-md-4" align="center" style="padding:20px;">
-                  <a href="peliculas.php?genero=<?php echo $r['nombrecategoria'];?>"><img src="<?php echo $r['imagen'];?>" style="width:60%;border-radius:20px"></a>
+         ?>     <div class="col-md-4" align="center" style="padding:20px;">
+                  <h4><strong style="color:white"><?php echo $r['nombrecategoria'];?></strong></h4>
+                  <a href="peliculas.php?genero=<?php echo $r['nombrecategoria'];?>"><img src="categorias/<?php echo $r['imagen'];?>" style="width:60%;border-radius:20px"></a>
                 </div>
          <?php }?>
        </div>
    </div>
  </body>
 </html>
+
+
